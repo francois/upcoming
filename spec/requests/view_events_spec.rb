3fea868c888ae79f610d5bfce43175c0a2288275
François Beausoleil <francois@teksol.info>
2011-04-19 19:00:03 -0400

    wip

--- spec/requests/view_events_spec.rb
+++ spec/requests/view_events_spec.rb
@@ -18,18 +18,18 @@ feature "View Events" do
     end
   end
 
-#  scenario "when there are many events" do
-#    Event.create!(:name => "Montreal.rb", :start_at => "2011-09-13 18:30-0400", :url => "http://montrealonrails.com/")
-#    Event.create!(:name => "PyMtl", :start_at => "2011-09-14 20:00-0400", :url => "http://pymontreal.com/")
-#
-#    visit "/"
-#    page.should have_css(".upcoming.events")
-#    within(:css, ".upcoming.events") do
-#      page.should have_css("li", :text => "Montreal.rb", :count => 1)
-#      page.should link_to("http://montrealonrails.com/")
-#
-#      page.should have_css("li", :text => "PyMtl", :count => 1)
-#      page.should link_to("http://pymontreal.com/")
-#    end
-#  end
+  scenario "when there are many events" do
+    mtlrb = Event.create!(:name => "Montreal.rb", :start_at => "2011-09-13 18:30-0400", :url => "http://montrealonrails.com/")
+    pymtl = Event.create!(:name => "PyMtl", :start_at => "2011-09-14 20:00-0400", :url => "http://pymontreal.com/")
+
+    visit "/"
+    page.should have_css(".upcoming.events")
+    within(:css, ".upcoming.events") do
+      page.should have_css("li", :text => "Montreal.rb", :count => 1)
+      page.should have_link_to(event_path(mtlrb))
+
+      page.should have_css("li", :text => "PyMtl", :count => 1)
+      page.should have_link_to(event_path(pymtl))
+    end
+  end
 end
